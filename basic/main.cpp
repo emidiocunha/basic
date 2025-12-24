25ede9145082d8aefabdb1dcb5d0db78a060610b
Emídio Cunha <emidiocunha@gmail.com>
2025-12-24 17:38:02 +0000

    Initial Commit

---
+++ basic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  basic
+//
+//  Created by Emidio Cunha on 24/12/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
